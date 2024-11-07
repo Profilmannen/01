253f0e77f31ab0008b042cdb3e050e6f7d673db4
Albin <albiv.forsberg2@elev.ga.ntig.se>
2024-11-07 16:25:12 +0100

    01

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿if( 6 >= 3)
+{
+    Console.WriteLine("Hej Världen!");
+}
+
+Console.WriteLine("Vad heter du?");
+
+string namn = Console.ReadLine();
+
+Console.WriteLine("Vad är lösenordet?");
+
+string password = Console.ReadLine();
+
+while( password != "12345")
+{
+    Console.WriteLine("Dra åt helvete! Fel namn eller lösenord");
+    password = Console.ReadLine(); 
+}
+
+if( namn == "kalleanka" && password == "12345")
+{
+    Console.WriteLine("Välkommen!");
+}
+
+// for(int i=1; i<=32; i++)
+// {
+//     Console.WriteLine("Hej igen världen!");
+// }
+
+Console.ReadLine();
